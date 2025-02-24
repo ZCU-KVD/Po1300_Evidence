ec0eca69d3a2d4ee03c98226695ce4fdf802166e
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2025-02-24 14:33:06 +0100

    Přidejte soubory projektu.

---
+++ Po1300_Evidence/Models/Polozka.cs
@@ -0,0 +1,18 @@
+﻿namespace Po1300_Evidence.Models
+{
+	public class Polozka
+	{
+		public Polozka() 
+		{
+			Datum = DateOnly.FromDateTime(DateTime.Now);
+		}
+		private double vynosy;
+		private double naklady;
+
+		public DateOnly Datum { get; set; }
+		public double Vynosy { get => vynosy; set => vynosy = Math.Round(Math.Abs(value),2); }
+		public double Naklady { get => naklady; set => naklady = Math.Round(Math.Abs(value), 2); }
+		public string Popis { get; set; }
+		public double Zisk => Vynosy - Naklady;
+	}
+}
---
+++ Po1300_Evidence/Pages/EvidenceZisku.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace Po1300_Evidence.Pages
+{
+	public partial class EvidenceZisku
+	{
+		#region Vlastnosti
+		/// <summary>
+		/// Seznam položek, kde uzivatel muze pridavat, upravovat a mazat jednotlive polozky
+		/// </summary>
+		private List<Models.Polozka> Polozky { get; set; } = new List<Models.Polozka>();
+		/// <summary>
+		/// Aktualni zadavana polozka
+		/// </summary>
+		public Models.Polozka Polozka { get; set; } = new Models.Polozka();
+		#endregion
+
+		#region Metody
+		private void Pridat()
+		{
+			Polozky.Add(Polozka);
+			
+		}
+		#endregion
+
+	}
+}
