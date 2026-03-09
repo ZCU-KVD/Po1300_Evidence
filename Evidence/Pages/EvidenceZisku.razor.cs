18d086cd105f53886fc4ec890f8bc670025050c3
Zdeněk Ulrych <zdenek.ulrych@outlook.com>
2026-03-09 14:29:44 +0100

    Přidána evidence nákladů a výnosů s generováním dat
    Zavedena služba EvidenceService pro správu transakcí a generování testovacích dat. EvidenceZisku.razor nyní zobrazuje tabulku transakcí s akcemi Upravit/Smazat a barevným zvýrazněním zisku. EvidenceService je injektována do komponenty, importy byly rozšířeny a přidán Material Symbols font pro ikony. Vytvořeny základy pro správu a zobrazení nákladů a výnosů.

---
+++ Evidence/Pages/EvidenceZisku.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Evidence.Pages
+{
+	public partial class EvidenceZisku
+	{
+		[Inject] private Services.EvidenceService EvidenceService { get; set; } = default!;
+
+		protected override void OnInitialized()
+		{
+			 if (EvidenceService.Transakce.Count == 0)
+			{
+				 EvidenceService.VygenerovatNahodnaData(30);
+			}
+		}
+	}
+}
---
+++ Evidence/Services/EvidenceService.cs
@@ -0,0 +1,27 @@
+﻿using Evidence.Models;
+
+namespace Evidence.Services
+{
+	public class EvidenceService
+	{
+		public List<Transakce> Transakce { get; set; } = new List<Transakce>();
+
+		public void VygenerovatNahodnaData(int pocet)
+		{
+			var random = new Random();
+			string[] popisy = { "Prodej zboží", "Konzultace", "Služby", "Oprava", "Pronájem" };
+
+			for (int i = 0; i < pocet; i++)
+			{
+				var transakce = new Transakce
+				{
+					Datum = DateOnly.FromDateTime(DateTime.Now.AddDays(-random.Next(0, 365))),
+					Popis = popisy[random.Next(popisy.Length)],
+					Vynosy = random.Next(1000, 50000),
+					Naklady = random.Next(500, 40000)
+				};
+				Transakce.Add(transakce);
+			}
+		}
+	}
+}
